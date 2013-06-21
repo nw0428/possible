2b83ef8097f51655317289b01e4be4200d0e507b
David Zabner <david.zabner@gmail.com>
2013-06-21 01:31:45 -0400

    aggar

--- config/application.rb
+++ config/application.rb
@@ -69,7 +69,7 @@ module Possible
     # Enable the asset pipeline
     config.assets.enabled = false
 
-    
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
